2835f1143df2526f0595a07b76656d6d8fb94731
JulianTs0 <julianelcapo19@gmail.com>
2024-05-26 17:21:11 -0300

    rebuild_DB_tryCatch

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ import mysql.connector
 from decouple import config
 
 
+
 def conect_DB():
     try:
         conection = mysql.connector.connect(user=config("USER_DB"),
@@ -11,18 +12,25 @@ def conect_DB():
                                             port=config("PORT_DB"))
         
         return conection
+
     except:
 
         print(f"Error DB connect {mysql.connector.Error}")
-
         return "Error al conectarse a la base de datos"
 
 
 
 def insert_user(user):
+
     state_register_user = "desconectado"
-    try:
-        conect = conect_DB()
+    conect = conect_DB()
+
+    if isinstance(conect,str):
+        print(f"Error al ingresar el ususario {conect}")
+        return "Error al ingresar el ususario"
+
+    else:
+
         cursor = conect.cursor()
         sql = "insert into usuarios values(null,%s,%s,%s,%s);"
         data = (user.name, user.password, user.email, state_register_user)
@@ -31,54 +39,58 @@ def insert_user(user):
         print(cursor.rowcount,"Usuario ingresado")
         conect.close()
 
-        return None
-    except:
-        print(f"Error al ingresar el ususario {mysql.connector.Error}")
-
-        return "Error al ingresar el ususario"
-
 
 
 def select_user():
-    try:
-        conect = conect_DB()
+
+    conect = conect_DB()
+
+    if isinstance(conect,str):
+        print(f"Error al mostrar los datos del ususario {conect}") 
+        return "Error al mostrar los datos del ususario"
+
+    else:
+
         cursor = conect.cursor()
         cursor.execute("select nombre, contra, email, estado from usuarios;")
         data = cursor.fetchall()
         conect.commit()
         conect.close()
-        return data
-
-    except: 
-        print(f"Error al mostrar los datos del ususario {mysql.connector.Error}") 
-
-        return "Error al mostrar los datos del ususario"
+        return data 
 
 
 
 def insert_order(order,user):
+
     state_order_user = "Espera"
-    try:
-        conect = conect_DB()
+    conect = conect_DB()
+
+    if isinstance(conect,str):
+        print(f"Error al ingresar el Pedido {conect}")
+        return "Error al ingresar el Pedido"
+
+    else:
+
         cursor = conect.cursor()
         sql = "insert into pedidos values(null,%s,%s,%s,%s,%s,%s,%s);"
         data = (user.name,order.ropa,order.servicio,order.prioridad,order.comentario,order.precio,state_order_user)
         cursor.execute(sql,data)
         conect.commit()
         print(cursor.rowcount,"Pedido Creado")
-        conect.close()
-
-        return None
-    except:
-        print(f"Error al ingresar el Pedido {mysql.connector.Error}")
-
-        return "Error al ingresar el Pedido"
+        conect.close() 
 
 
 
 def select_order():
-    try:
-        conect = conect_DB()
+
+    conect = conect_DB()
+
+    if isinstance(conect,str):
+        print(f"Error al mostrar los peiddos del ususario {conect}")
+        return "Error al mostrar los pedidos del ususario" 
+
+    else:
+
         cursor = conect.cursor()
         cursor.execute("select id, usuario, ropa, servicio, prioridad, precio, estado from pedidos;")
         data = cursor.fetchall()
@@ -86,56 +98,60 @@ def select_order():
         conect.close()
         return data
 
-    except:
-        print(f"Error al mostrar los peiddos del ususario {mysql.connector.Error}")
-
-        return "Error al mostrar los pedidos del ususario"
-
 
 
 def delete_order_db(id):
-    try:
-        conect = conect_DB()
+
+    conect = conect_DB()
+
+    if isinstance(conect,str):
+
+        print(f"Error al Eliminar el pedido {conect}")
+        return "Error al Eliminar el pedido"
+    else:
+
         cursor = conect.cursor()
         sql = "delete from pedidos where pedidos.id = %s;"
         data = (id,)
         cursor.execute(sql,data)
         conect.commit()
         print(cursor.rowcount,"Pedido Eliminado")
-        conect.close()
-
-        return None
-    except:
-        print(f"Error al Eliminar el pedido {mysql.connector.Error}")
-
-        return "Error al Eliminar el pedido"
+        conect.close() 
 
 
 
 def user_state_switch(user_name,state):
-    if state:
-        state = "conectado"
-    else:
-        state = "desconectado"
 
     conect = conect_DB()
-    cursor = conect.cursor()
-    sql = "update usuarios set usuarios.estado = %s WHERE usuarios.nombre = %s;"
-    data = (state,user_name)
-    cursor.execute(sql,data)
-    conect.commit()
-    print(cursor.rowcount,f"Usuario {state}")
-    conect.close()
 
-    return
+    if isinstance(conect,str):
+
+        print(f"Error al cambiar el estado del usuario {conect}")
+        return "Error al Eliminar el pedido"
+    else:
+
+        state = "desconectado"
+        if state:
+            state = "conectado"
+        cursor = conect.cursor()
+        sql = "update usuarios set usuarios.estado = %s WHERE usuarios.nombre = %s;"
+        data = (state,user_name)
+        cursor.execute(sql,data)
+        conect.commit()
+        print(cursor.rowcount,f"Usuario {state}")
+        conect.close() 
 
 
 
 def get_service_stock_data(stock_table):
 
-    try:
+    conect = conect_DB()
+    if isinstance(conect,str):
+        print(f"Error al mostrar los datos del ususario {conect}")
+        return "Error al mostrar el stock"
+    
+    else:
 
-        conect = conect_DB()
         cursor = conect.cursor()
         cursor.execute(f"select nombre, precio from {stock_table};")
         data = cursor.fetchall()
@@ -143,8 +159,3 @@ def get_service_stock_data(stock_table):
         conect.close()
         
         return data
-
-    except:
-        print(f"Error al mostrar los datos del ususario {mysql.connector.Error}")
-
-        return "Error al mostrar el stock"
